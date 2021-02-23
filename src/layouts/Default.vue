43dfbe8c0ce78909a8cd464dbf608a7f40ef1b0e
theoria <theoria23@gmail.com>
2021-02-24 08:38:59 +0900

    first commit

---
+++ src/layouts/Default.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="layout">
+    <header class="header">
+      <h1>
+        <g-link to="/">{{ $static.metadata.siteName }}</g-link>
+      </h1>
+    </header>
+    <slot />
+    <footer>
+      <hr />
+      <div>
+        このページのソースコードは
+        <g-link to="https://github.com/theoria24/assaultli.ly">GitHub</g-link>
+        上で管理されています。修正の必要がある場合は
+        <g-link to="https://github.com/theoria24/assaultli.ly/issues/new"
+          >Issue</g-link
+        >
+        や
+        <g-link to="https://github.com/theoria24/assaultli.ly/compare"
+          >Pull request</g-link
+        >
+        をお願いします。
+      </div>
+      <hr />
+      <div>
+        &copy; 2020<!-- -{{ new Date().getFullYear() }}-->
+        assaultli.ly
+      </div>
+    </footer>
+  </div>
+</template>
+
+<static-query>
+query {
+  metadata {
+    siteName
+  }
+}
+</static-query>
+
+<style>
+body {
+  font-family: -apple-system, system-ui, BlinkMacSystemFont, "Segoe UI", Roboto,
+    "Helvetica Neue", Arial, sans-serif;
+  margin: 0;
+  padding: 0;
+  line-height: 1.5;
+}
+
+.layout {
+  max-width: 760px;
+  margin: 0 auto;
+  padding-left: 20px;
+  padding-right: 20px;
+}
+
+.header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+  height: 80px;
+}
+
+.nav__link {
+  margin-left: 20px;
+}
+</style>
---
+++ src/pages/Index.vue
@@ -0,0 +1,225 @@
+<template>
+  <Layout>
+    <section>
+      <p>
+        このサイトは非公式です。アサルトリリィプロジェクトやその関係者に問い合わせを行うことはお控えください。
+        (連絡先: admin@assaultli.ly)
+      </p>
+    </section>
+    <section>
+      <h2>
+        <svg
+          viewBox="0 0 15 15"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+        >
+          <path
+            d="M7.5.5l.325-.38a.5.5 0 00-.65 0L7.5.5zm-7 6l-.325-.38L0 6.27v.23h.5zm5 8v.5a.5.5 0 00.5-.5h-.5zm4 0H9a.5.5 0 00.5.5v-.5zm5-8h.5v-.23l-.175-.15-.325.38zM1.5 15h4v-1h-4v1zm13.325-8.88l-7-6-.65.76 7 6 .65-.76zm-7.65-6l-7 6 .65.76 7-6-.65-.76zM6 14.5v-3H5v3h1zm3-3v3h1v-3H9zm.5 3.5h4v-1h-4v1zm5.5-1.5v-7h-1v7h1zm-15-7v7h1v-7H0zM7.5 10A1.5 1.5 0 019 11.5h1A2.5 2.5 0 007.5 9v1zm0-1A2.5 2.5 0 005 11.5h1A1.5 1.5 0 017.5 10V9zm6 6a1.5 1.5 0 001.5-1.5h-1a.5.5 0 01-.5.5v1zm-12-1a.5.5 0 01-.5-.5H0A1.5 1.5 0 001.5 15v-1z"
+            fill="currentColor"
+          ></path>
+        </svg>
+        公式サイト
+      </h2>
+      <ul>
+        <li>
+          <g-link to="https://assaultlily-pj.com/"
+            >アサルトリリィ プロジェクトポータルサイト</g-link
+          >
+        </li>
+        <li>
+          <g-link to="https://assaultlily.com/"
+            >Assault Lily (アクションドール)</g-link
+          >
+        </li>
+        <li>
+          <g-link to="https://anime.assaultlily-pj.com/">
+            アサルトリリィBOUQUET(ブーケ) アニメ公式サイト
+          </g-link>
+        </li>
+        <li>
+          <g-link to="https://assaultlily.bushimo.jp/">
+            アサルトリリィ Last Bullet (ラスバレ) 公式サイト
+          </g-link>
+        </li>
+      </ul>
+    </section>
+    <section>
+      <h2>
+        <svg
+          viewBox="0 0 15 15"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+        >
+          <path
+            d="M14.478 1.5l.5-.033a.5.5 0 00-.871-.301l.371.334zm-.498 2.959a.5.5 0 10-1 0h1zm-6.49.082h-.5.5zm0 .959h.5-.5zm-6.99 7V12a.5.5 0 00-.278.916L.5 12.5zm.998-11l.469-.175a.5.5 0 00-.916-.048l.447.223zm3.994 9l.354.353a.5.5 0 00-.195-.827l-.159.474zm7.224-8.027l-.37.336.18.199.265-.04-.075-.495zm1.264-.94c.051.778.003 1.25-.123 1.606-.122.345-.336.629-.723 1l.692.722c.438-.42.776-.832.974-1.388.193-.546.232-1.178.177-2.006l-.998.066zm0 3.654V4.46h-1v.728h1zm-6.99-.646V5.5h1v-.959h-1zm0 .959V6h1v-.5h-1zM10.525 1a3.539 3.539 0 00-3.537 3.541h1A2.539 2.539 0 0110.526 2V1zm2.454 4.187C12.98 9.503 9.487 13 5.18 13v1c4.86 0 8.8-3.946 8.8-8.813h-1zM1.03 1.675C1.574 3.127 3.614 6 7.49 6V5C4.174 5 2.421 2.54 1.966 1.325l-.937.35zm.021-.398C.004 3.373-.157 5.407.604 7.139c.759 1.727 2.392 3.055 4.73 3.835l.317-.948c-2.155-.72-3.518-1.892-4.132-3.29-.612-1.393-.523-3.11.427-5.013l-.895-.446zm4.087 8.87C4.536 10.75 2.726 12 .5 12v1c2.566 0 4.617-1.416 5.346-2.147l-.708-.706zm7.949-8.009A3.445 3.445 0 0010.526 1v1c.721 0 1.37.311 1.82.809l.74-.671zm-.296.83a3.513 3.513 0 002.06-1.134l-.744-.668a2.514 2.514 0 01-1.466.813l.15.989zM.222 12.916C1.863 14.01 3.583 14 5.18 14v-1c-1.63 0-3.048-.011-4.402-.916l-.556.832z"
+            fill="currentColor"
+          ></path>
+        </svg>
+        Twitter
+      </h2>
+      <ul>
+        <li>
+          <g-link to="https://twitter.com/assaultlily_pj"
+            >アサルトリリィ公式 (@assaultlily_pj)</g-link
+          >
+        </li>
+        <li>
+          <g-link to="https://twitter.com/assaultlily_pj"
+            >アサルトリリィ Last Bullet 公式 (@assaultlily_lb)</g-link
+          >
+        </li>
+        <li>
+          <g-link to="https://twitter.com/assault_lily"
+            >二川二水@アサルトリリィ原作公式 (@assault_lily)</g-link
+          >
+        </li>
+      </ul>
+    </section>
+    <section>
+      <h2>
+        <svg
+          viewBox="0 0 15 15"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+        >
+          <path
+            d="M9.5 14.5h4a1 1 0 001-1v-12a1 1 0 00-1-1h-4a1 1 0 00-1 1v12a1 1 0 001 1zm0 0H4m2.5-3v3m2-9h6m-4.5 6h3m-11.5-8h7v8h-7a1 1 0 01-1-1v-6a1 1 0 011-1z"
+            stroke="currentColor"
+          ></path>
+        </svg>
+        メディア
+      </h2>
+      <ul>
+        <li>
+          <g-link to="https://www.youtube.com/channel/UCBBWEbD8FflD5g3Ya_dRV-g"
+            >[YouTube] アサルトリリィプロジェクト公式チャンネル</g-link
+          >
+        </li>
+        <li>
+          <g-link to="https://www.youtube.com/channel/UCAI3Ng1g4ZVSJZqdEL7YRHw"
+            >[YouTube] Assalt lily-アサルトリリィ-</g-link
+          >
+        </li>
+        <li>
+          <g-link to="https://hibiki-radio.jp/description/assaultlily/detail"
+            >[HiBiKi Radio Station] アサルトリリィ
+            ラジオガーデン‐GROWING‐</g-link
+          >
+        </li>
+        <li>
+          <g-link to="https://www.tbs.co.jp/anime/assaultlily-pj/"
+            >[TBSテレビ] アサルトリリィBOUQUET(ブーケ)</g-link
+          >
+        </li>
+        <li>
+          <g-link to="https://www.bs-tbs.co.jp/anime/assaultlily/"
+            >[BS-TBS] アサルトリリィ BOUQUET</g-link
+          >
+        </li>
+        <li>
+          <g-link to="https://www.tbsradio.jp/assaultlily/"
+            >[TBSラジオ] アサルトリリィラジオガーデン‐GROWING‐</g-link
+          >
+        </li>
+      </ul>
+    </section>
+    <section>
+      <h2>
+        <svg
+          viewBox="0 0 15 15"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+        >
+          <path
+            d="M5.5 3v-.5a2 2 0 114 0V3m-9 3.5c3.706 4.235 10.294 4.235 14 0m-13-3h12a1 1 0 011 1v9a1 1 0 01-1 1h-12a1 1 0 01-1-1v-9a1 1 0 011-1z"
+            stroke="currentColor"
+          ></path>
+        </svg>
+        アサルトリリィプロジェクト
+      </h2>
+      <ul>
+        <li>
+          <g-link to="https://bushiroad.com/">ブシロード</g-link>
+        </li>
+        <li>
+          <g-link to="https://www.tbs.co.jp/">TBSテレビ</g-link>
+        </li>
+        <li>
+          <g-link to="https://pokelabo.co.jp/">ポケラボ</g-link>
+        </li>
+        <li>
+          <g-link to="http://www.shaft-web.co.jp/">シャフト</g-link>
+        </li>
+        <li>
+          <g-link to="https://www.azone-int.co.jp/"
+            >アゾンインターナショナル</g-link
+          >
+        </li>
+        <li>
+          <g-link to="https://www.0101maruigroup.co.jp/">丸井グループ</g-link>
+        </li>
+        <li>
+          <g-link to="https://bushiroad-creative.com/"
+            >ブシロードクリエイティブ</g-link
+          >
+        </li>
+        <li>
+          <g-link to="https://bushiroad-music.com/"
+            >ブシロードミュージック</g-link
+          >
+        </li>
+      </ul>
+      <section>
+        <h2>その他</h2>
+        <ul>
+          <li>
+            <g-link to="https://twitter.com/obanazawa_acus"
+              >[Twitter] 尾花沢軒栄 (@obanazawa_acus)</g-link
+            >
+          </li>
+          <li>
+            <g-link to="https://acus.booth.pm/"
+              >[BOOTH] acusオフィシャルショップ</g-link
+            >
+          </li>
+          <li>
+            <g-link to="http://for.fool.jp/ludojyo/wp/"
+              >私立ルドビコ女学院公式WEBサイト</g-link
+            >
+          </li>
+          <li>
+            <g-link to="https://twitter.com/ludojyo"
+              >[Twitter] 私立ルドビコ女学院 (@ludojyo)</g-link
+            >
+          </li>
+          <li>
+            <g-link to="https://hobby.dengeki.com/tag/assaultlily/"
+              >[電撃ホビーウェブ] アサルトリリィ</g-link
+            >
+          </li>
+          <li>
+            <g-link to="https://hobby.dengeki.com/assaultlily-2/"
+              >[電撃ホビーウェブ] アサルトリリィ
+              真島百由の超兵器工房～御台場女学校編</g-link
+            >
+          </li>
+        </ul>
+      </section>
+    </section>
+  </Layout>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
